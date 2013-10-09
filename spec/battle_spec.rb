35d18fca863162e4702f4f867d7991dd25fc40ab
David Bella <dbella1@gmail.com>
2013-10-09 17:12:11 -0400

    Created quick battle classes and basic test spec

---
+++ spec/battle_spec.rb
@@ -0,0 +1,24 @@
+require 'guard/notifier'
+require 'simplecov'
+SimpleCov.start do
+  SimpleCov.at_exit do
+    SimpleCov.result.format!
+
+    result = SimpleCov.result
+    percentage = (result.covered_lines / Float(result.total_lines) * 100).to_i
+    new_result = __FILE__.split('/').last
+    new_result += "\n" + result.covered_lines.to_s
+    new_result += " / " + result.total_lines.to_s
+    new_result += " lines - " + percentage.to_s + "%"
+
+    Guard::Notifier.notify(
+      new_result,
+      :title => "SimpleCov",
+      :image => percentage < 100 ? :failed : :success
+    )
+  end
+end
+
+require 'json'
+require_relative './spec_helper'
+require_relative '../lib/battle'
