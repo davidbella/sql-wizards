53a1c85141fb1c772dd78de1a94cd0003c8ea5f8
David Bella <dbella1@gmail.com>
2013-10-11 15:59:56 -0400

    Updates to the battle class to let it take over game control

--- lib/battle.rb
+++ lib/battle.rb
@@ -1,5 +1,32 @@
 class Battle
-  def initialize(player, opponent)
+  attr_accessor :battling, :player, :opponent
 
+  def initialize(player, opponent)
+    @player = player
+    @opponent = opponent
+    @battling = true
+  end
+
+  def battling?
+    self.battling
+  end
+
+  def output(msgs)
+    msgs.map do |msg|
+      puts "🔨  " + msg
+    end
+  end
+
+  def get_command
+    while (battling?)
+      output(["You are in a fight!"])
+      cmd = gets.strip.downcase
+      self.send(cmd)
+    end
+  end
+
+  def attack
+    player.cast(player.spells[:spell0], opponent)
+    p opponent
   end
 end
